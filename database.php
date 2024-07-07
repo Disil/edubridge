494bbbca6b89aaaf16a617d01d822f4075070ac7
Disil <satrianugraha2007@gmail.com>
2024-07-07 20:34:45 +0700

    (true fix): membuat koneksi db_user baru dan menambahkan port

--- database.php
+++ database.php
@@ -1,11 +1,12 @@
 <?php
 $db_server = "edubridge.my.id";
-$db_user = "wpcguvfn";
-$db_pass = "S@tri@2007";
+$db_user = "wpcguvfn_user";
+$db_pass = "EduBridgeID";
 $db_name = "wpcguvn_edubridge_db";
+$db_port = "3306";
 $conn = "";
 
-$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+$conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name, $db_port);
 
 if ($conn->connect_error) {
     echo "Connection failed: " . $conn->connect_error;
