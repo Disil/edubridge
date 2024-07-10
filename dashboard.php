21dd847dadb2c50bcf5fffd286a9a820ad869961
Satria Nugraha <satrianugraha2007@gmail.com>
2024-07-10 11:54:09 +0700

    feat: new landing page with illustration refactor: now using buttons instead of pure link where it's possible

--- dashboard.php
+++ dashboard.php
@@ -20,11 +20,9 @@
         <h1>Dashboard</h1>
         <p>Selamat datang, <?php echo $_SESSION['nama_siswa']?>! </p>
         <p>Untuk mendapatkan rekomendasi jurusan, silahkan mulai dari mengisi nilai rapot berdasarkan semester terbaru, lalu dilanjutkan dengan tes RIASEC untuk mengukur bakat dan minat. Setelah itu, anda bisa melihat hasil rekomendasi jurusan beserta dengan penjelasannya.</p>
-        <ul>
-            <li><a href="nilai_rapot.php">Input Nilai Rapot</a></li>
-            <li><a href="tes_riasec_info.php">Tes RIASEC</a></li>
-            <li><a href="hasil_rekomendasi.php">Lihat hasil rekomendasi</a></li>
-        </ul>
+        <button onclick="window.location.href='input_nilai_rapot.php';">Input Nilai Rapot</button><br>
+        <button onclick="window.location.href='tes_riasec_info.php';">Tes RIASEC</button><br>
+        <button onclick="window.location.href='hasil_rekomendasi.php';">Lihat hasil rekomendasi</button>
     </main>
 <footer><?php include 'structure/footer.php'; ?></footer>
 </body>
--- index.php
+++ index.php
@@ -13,22 +13,26 @@ if (isset($_GET['logged_out'])) {
     <link rel="stylesheet" href="css/classless.css">
     <link rel="stylesheet" href="css/tabbox.css">
     <link rel="stylesheet" href="css/themes.css">
-    <title>EduBridge</title>
+    <title>EduBridge - landing page</title>
 </head>
 <body>
 <header>
     <?php include 'structure/header_no_account.php'; ?>
 </header>
 <main>
-    <figure><img alt = "edubridge logo" src="img/edubridge_logo1.png"></figure>
-    <p>Selamat datang di EduBridge. Silahkan login atau register untuk melanjutkan.</p>
-    <p>EduBridge adalah situs yang dirancang untuk membantu kamu dalam menentukan pilihan jurusan di perguruan tinggi. Caranya adalah dengan memasukkan nilai rapot serta melakukan tes minat bakat yang berbentuk tes RIASEC.</p>
-    <p>Setelah itu, kamu akan mendapatkan rekomendasi jurusan yang sesuai dengan nilai rapot dan tes minat bakat yang kamu kerjakan.</p>
-    <p>Jika kamu belum pernah memakai EduBridge sebelumnya, silahkan buat akun terlebih dahulu.</p>
-    <ul>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Buat Akun Baru</a></li>
-    </ul>
+    <h1>EduBridge - Situs Pemilihan Jurusan Perguruan Tinggi</h1>
+    <div class="row">
+        <div class="col">
+            <p>EduBridge adalah situs yang dirancang untuk membantu kamu dalam menentukan pilihan jurusan di perguruan tinggi. Caranya adalah dengan memasukkan nilai rapot serta melakukan tes minat bakat yang berbentuk tes RIASEC.</p>
+            <p>Setelah itu, kamu akan mendapatkan rekomendasi jurusan yang sesuai dengan nilai rapot dan tes minat bakat yang kamu kerjakan.</p>
+            <p>Jika kamu belum pernah memakai EduBridge sebelumnya, silahkan buat akun terlebih dahulu.</p>
+            <button onclick="window.location.href='register.php';">Buat Akun Baru</button><br>
+            <button onclick="window.location.href='login.php';">Login Akun</button>
+        </div>
+        <div class="col">
+            <figure><img alt = "illustration pc" src="img/orange-web-design-on-laptop-with-color-wheel.png"></figure>
+        </div>
+    </div>
 </main>
 <footer><?php include 'structure/footer.php'; ?></footer>
 </body>
