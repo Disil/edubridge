d9d49b8875c0951eb0904601586eae0fd120d1e8
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-02 21:29:58 +0700

    new: info jurusan farmasi

---
+++ jurusan/farmasi.php
@@ -0,0 +1,37 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Farmasi dan Kesehatan Masyarakat</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Farmasi dan Kesehatan Masyarakat</h1>
+    <h2>Deskripsi</h2>
+    <h3>1. Farmasi</h3>
+    <p>Mahasiswa Farmasi mempelajari ilmu tentang obat-obatan, termasuk pengembangan, produksi, pengujian, dan distribusi. Mereka juga belajar tentang interaksi obat dengan tubuh manusia dan bagaimana meresepkan serta memberikan obat yang aman dan efektif. Lulusan farmasi dapat bekerja di apotek, rumah sakit, industri farmasi, dan penelitian.</p>
+    <h3>2. Kesehatan Masyarakat (kesmas)</h3>
+    <p>Mahasiswa Kesehatan Masyarakat mempelajari cara-cara untuk meningkatkan kesehatan masyarakat melalui pendidikan, kebijakan, dan penelitian. Mereka dilatih untuk menganalisis data kesehatan, mengembangkan program kesehatan, dan mengimplementasikan kebijakan kesehatan yang efektif. Lulusan dapat bekerja di lembaga pemerintah, organisasi non-profit, rumah sakit, dan institusi penelitian.</p>
+    <h2>Prospek Kerja</h2>
+    <h3>1. Farmasi</h3>
+    <p>Prospek kerja lulusan Farmasi sangat baik, karena permintaan akan tenaga farmasiselalu tinggi. Lulusan farmasi dapat bekerja di apotek, rumah sakit, industri farmasi, dan penelitian. Mereka dapat bekerja sebagai apoteker, peneliti obat, manajer farmasi, dan konsultan farmasi.</p>
+    <h3>2. Kesehatan Masyarakat (kesmas)</h3>
+    <p>Prospek kerja lulusan Kesehatan Masyarakat juga sangat baik, karena permintaan akan tenaga kesehatan masyarakat terus meningkat. Lulusan kesehatan masyarakat dapat bekerja di lembaga pemerintah, organisasi non-profit, rumah sakit, dan institusi penelitian. Mereka dapat bekerja sebagai analis data kesehatan, manajer program kesehatan, peneliti kesehatan masyarakat, dan konsultan kesehatan masyarakat.</p>
+</main>
+<footer>
+    <?php include '../structure/footer.php'; ?>
+</footer>
+</body>
+</html>
