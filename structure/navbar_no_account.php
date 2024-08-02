6bb3d25dddb35d92179e47ca74122e5300d8b5a6
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-02 21:54:05 +0700

    new: emoji in navbar_no_account

--- structure/navbar_no_account.php
+++ structure/navbar_no_account.php
@@ -2,9 +2,10 @@
 <nav>
     <ul>
         <li><img alt = "edubridge logo" src="../img/edubridge_logo1.png" class="icon">EduBridge</li>
-        <li class="float-right sticky"><a href="../login.php">Login</a></li>
-        <li><a href="../index.php">Beranda</a></li>
-        <li><a href="#">Kontak </a></li>
-        <li class="float-right"><a href="../register.php">Daftar</a></li>
+        <li class="float-right sticky"><a href="../login.php"><img src="../img/icon/icons8-enter-50.svg" alt="edit" class="icon">Login</a></li>
+        <li><a href="../index.php">🏠 Beranda</a></li>
+        <li><a href="#">☎️ Kontak</a></li>
+        <li><a href="../cara_kerja.php">🌏 Tentang</a></li>
+        <li class="float-right"><a href="../register.php">✨ Daftar</a></li>
     </ul>
 </nav>
