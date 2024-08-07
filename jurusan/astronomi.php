9e25e8f8d7fe8865b6e068f23a8d79ed663214e2
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-07 13:17:35 +0700

    new: penjelasan jurusan astronomi

---
+++ jurusan/astronomi.php
@@ -0,0 +1,34 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Astronomi</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Astronomi</h1>
+    <h2>Deskripsi</h2>
+    <p>Jurusan ini berfokus pada studi tentang objek dan fenomena di luar Bumi, seperti bintang, planet, galaksi, dan kosmos. Kamu akan mempelajari prinsip-prinsip fisika dan matematika yang digunakan untuk memahami fenomena astronomis, serta cara memantau dan menganalisis data dari teleskop dan instrumen astronomi.</p>
+    <h2>Prospek Kerja</h2>
+    <p>Lulusan jurusan Astronomi memiliki peluang karir di berbagai bidang, termasuk:</p>
+    <ul>
+        <li>Peneliti Astronomi: Melakukan penelitian tentang benda langit dan fenomena astronomis.</li>
+        <li>Pengajar Astronomi: Mengajar di perguruan tinggi atau lembaga pendidikan lainnya.</li>
+        <li>Peneliti Ruang Angkasa: Bekerja di lembaga antariksa untuk misi penjelajahan ruang angkasa.</li>
+    </ul>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
