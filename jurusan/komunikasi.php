9b30381e36b8b1ee024bf63453c5ab7d63417a8b
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-07 13:21:25 +0700

    new: penjelasan jurusan komunikasi

---
+++ jurusan/komunikasi.php
@@ -0,0 +1,36 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Komunikasi</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Komunikasi</h1>
+    <h2>Deskripsi</h2>
+    <p>Jurusan Komunikasi mempelajari berbagai aspek penyampaian informasi dan pesan kepada publik melalui berbagai media dan platform. Kamu di jurusan ini akan mempelajari teknik-teknik komunikasi, strategi media, jurnalisme, pemasaran, hubungan masyarakat, dan komunikasi digital. Mereka juga diajarkan tentang cara mengelola dan merancang kampanye komunikasi yang efektif serta memahami dampak media dalam masyarakat.</p>
+    <h2>Prospek Karir</h2>
+    <p>Lulusan jurusan Komunikasi memiliki beragam peluang karir di berbagai industri, termasuk:</p>
+    <ul>
+        <li>Jurnalis: Menulis berita dan laporan untuk media cetak, online, atau penyiaran.</li>
+        <li>Manajer Pemasaran: Merancang strategi pemasaran dan promosi untuk produk dan merek.</li>
+        <li>Spesialis Hubungan Masyarakat: Menangani komunikasi dan hubungan publik untuk organisasi atau perusahaan.</li>
+        <li>Manajer Media Sosial: Mengelola dan merancang konten untuk platform media sosial.</li>
+        <li>Produser Konten: Bertanggung jawab atas produksi konten media seperti video, podcast, dan artikel.</li>
+    </ul>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
