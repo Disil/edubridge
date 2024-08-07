8898bb4a2d479b9e31cf807a8f2d720589845fbe
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-07 13:19:48 +0700

    new: penjelasan jurusan sejarah

---
+++ jurusan/antropologi.php
@@ -0,0 +1,44 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Antropologi dan Sejarah</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Antropologi dan Sejarah</h1>
+    <h2>Deskripsi</h2>
+    <p>Jurusan Antropologi dan Sejarah adalah bidang studi yang mempelajari manusia dan peradabannya dari masa lampau hingga masa kini. Antropologi adalah ilmu yang mempelajari manusia dalam segala aspeknya, termasuk budaya, sosial, dan biologis. Sejarah adalah ilmu yang mempelajari peristiwa dan perkembangan manusia dari masa lampau hingga sekarang. Mahasiswa di jurusan ini akan mempelajari berbagai teori, metode, dan konsep dalam antropologi dan sejarah untuk memahami kompleksitas manusia dan peradabannya.</p>
+    <h2>Spesialisasi</h2>
+    <p>Beberapa spesialisasi yang umum dalam jurusan Antropologi dan Sejarah meliputi:</p>
+    <ul>
+        <li>Antropologi Budaya: Studi tentang budaya manusia, termasuk nilai, norma, kepercayaan, dan praktik sosial.</li>
+        <li>Antropologi Sosial: Studi tentang struktur sosial, interaksi sosial, dan konflik sosial dalam masyarakat.</li>
+        <li>Arkeologi: Studi tentang sisa-sisa materi manusia dari masa lampau untuk memahami kehidupan dan budaya masa lalu.</li>
+        <li>Sejarah Dunia: Studi tentang peristiwa dan perkembangan manusia di berbagai belahan dunia.</li>
+        <li>Sejarah Indonesia: Studi tentang sejarah bangsa Indonesia dari masa prasejarah hingga masa kini.</li>
+    </ul>
+    <h2>Prospek Karir</h2>
+    <p>Lulusan jurusan Antropologi dan Sejarah memiliki beragam peluang karir di berbagai bidang, termasuk:</p>
+    <ul>
+        <li>Peneliti: Melakukan penelitian tentang budaya, sejarah, dan masyarakat.</li>
+        <li>Dosen: Mengajar di perguruan tinggi atau lembaga pendidikan lainnya.</li>
+        <li>Kurator Museum: Bertanggung jawab atas koleksi dan pameran museum.</li>
+        <li>Penulis Sejarah: Menulis buku, artikel, atau materi sejarah untuk publikasi.</li>
+    </ul>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
