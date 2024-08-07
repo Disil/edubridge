66387e1ea74ababf459b3ba2c395afc8bee069d2
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-07 13:14:58 +0700

    new: penjelasan jurusan meteorologi dan pariwisata

---
+++ jurusan/meteorologi.php
@@ -0,0 +1,40 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Meteorologi/Geologi</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Meteorologi/Geologi</h1>
+    <h2>Deskripsi</h2>
+    <h3>1. Meteorologi</h3>
+    <p>Meteorologi merupakan ilmu yang mempelajari tentang bumi dan berbagai gejalanya, termasuk tentang penyebab perubahan iklim dan cuaca serta hubungannya terhadap kelangsungan hidup manusia. Selain itu, kamu juga akan mendalami gejala-gejala alam seperti puting beliung, angin topan, dan sebagainya. Kamu pun nggak hanya mempelajari penyebab bencana tersebut bisa terjadi ya, tetapi kamu juga akan mengkaji mengenai solusi untuk menghadapi dan meminimalisir dampak dari berbagai bencana alam.</p>
+    <h3>2. Geologi</h3>
+    <p>Jurusan ini berfokus pada studi tentang sifat fisik bumi dan proses-prosesnya. Mahasiswa mempelajari struktur bumi, dinamika interior bumi, dan fenomena seperti gempa bumi dan aktivitas vulkanik. Kamu akan menggunakan teknik geofisika untuk eksplorasi sumber daya alam dan mitigasi risiko bencana alam.</p>
+    <h2>Spesialisasi</h2>
+    <p>Beberapa spesialisasi yang umum dalam jurusan Meteorologi/Geologi meliputi:</p>
+    <ul>
+        <li>Meteorologi: Spesialisasi dalam mempelajari cuaca dan iklim. Mahasiswa akan mempelajari tentang proses atmosfer, perubahan iklim, dan prediksi cuaca.</li>
+        <li>Geologi: Spesialisasi dalam mempelajari geologi bumi. Mahasiswa akan mempelajari tentang pembentukan bumi, struktur bumi, dan sejarah bumi.</li>
+        <li>Geofisika: Spesialisasi dalam mempelajari fenomena fisika di bumi. Mahasiswa akan mempelajari tentang gempa bumi, vulkanisme, dan proses geodinamika.</li>
+        <li>Geografi: Spesialisasi dalam mempelajari tentang peta dan lokasi. Mahasiswa akan mempelajari tentang peta, lokasi, dan fenomena geografis lainnya.</li>
+    </ul>
+    <h2>Prospek Karir</h2>
+    <p>Lulusan jurusan Meteorologi dan Geofisika memiliki peluang karir yang luas di berbagai sektor. Mereka dapat bekerja sebagai meteorolog, ahli klimatologi, geofisikawan, peneliti di lembaga penelitian, ahli mitigasi bencana, dan konsultan lingkungan. Selain itu, mereka dapat bekerja di industri minyak, gas, pertambangan, serta di lembaga pemerintah yang terkait dengan pemantauan cuaca dan mitigasi bencana alam. Pengetahuan dan keterampilan mereka sangat penting untuk memahami dan mengelola fenomena alam serta untuk eksplorasi dan pengelolaan sumber daya alam.</p>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
---
+++ jurusan/pariwisata.php
@@ -0,0 +1,45 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Pariwisata dan Perhotelan</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Pariwisata dan Perhotelan</h1>
+    <h2>Deskripsi</h2>
+    <p>Jurusan Pariwisata dan Perhotelan adalah bidang studi yang mempersiapkan mahasiswa untuk bekerja di industri pariwisata, perhotelan, dan perjalanan. Mahasiswa di jurusan ini akan mempelajari berbagai aspek pariwisata, manajemen perhotelan, dan layanan perjalanan, serta keterampilan yang diperlukan untuk menjadi profesional di bidang ini. Kamu akan belajar tentang destinasi pariwisata, manajemen hotel, pemasaran pariwisata, dan aspek lain yang terkait dengan industri pariwisata dan perhotelan.</p>
+    <h2>Spesialisasi</h2>
+    <p>Beberapa spesialisasi yang umum dalam jurusan Pariwisata dan Perhotelan meliputi sebagai berikut:</p>
+    <ul>
+        <li>Manajemen Hotel: Spesialisasi dalam manajemen operasional</li>
+        <li>Manajemen Restoran: Spesialisasi dalam manajemen restoran dan layanan makanan</li>
+        <li>Pemasaran Pariwisata: Spesialisasi dalam pemasaran destinasi pariwisata</li>
+        <li>Manajemen Acara: Spesialisasi dalam perencanaan dan pelaksanaan acara</li>
+        <li>Manajemen Perjalanan: Spesialisasi dalam manajemen perjalanan dan layanan perjalanan, contohnya SJB tour</li>
+    </ul>
+    <h2>Prospek Kerja</h2>
+    <p>Lulusan jurusan Pariwisata dan Perhotelan memiliki beragam peluang karir di industri pariwisata, perhotelan, dan perjalanan, termasuk:</p>
+    <ul>
+        <li>Manajer Hotel: Bertanggung jawab atas operasional hotel</li>
+        <li>Manajer Restoran: Bertanggung jawab atas operasional restoran</li>
+        <li>Spesialis Pemasaran Pariwisata: Bertanggung jawab atas pemasaran destinasi pariwisata</li>
+        <li>Manajer Acara: Bertanggung jawab atas perencanaan dan pelaksanaan acara</li>
+        <li>Manajer Perjalanan: Bertanggung jawab atas manajemen perjalanan dan layanan perjalanan</li>
+    </ul>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
