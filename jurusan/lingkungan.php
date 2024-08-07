51451994e6414413b3ff7df20d62542569062694
Satria Nugraha <satrianugraha2007@gmail.com>
2024-08-07 13:22:33 +0700

    new: penjelasan jurusan lingkungan

---
+++ jurusan/lingkungan.php
@@ -0,0 +1,35 @@
+<?php include '../structure/check_conn.php';
+include '../database.php'; ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/classless.css">
+    <link rel="stylesheet" href="../css/tabbox.css">
+    <link rel="stylesheet" href="../css/themes.css">
+    <title>EduBridge - Jurusan Lingkungan Hidup</title>
+</head>
+<body>
+<header>
+    <?php include '../structure/navbar.php'; ?>
+</header>
+<main>
+    <h1>Jurusan Lingkungan Hidup</h1>
+    <h2>Deskripsi</h2>
+    <p>Jurusan Lingkungan Hidup berfokus pada studi tentang interaksi antara manusia dan lingkungan, serta upaya untuk menjaga dan melestarikan keberlangsungan lingkungan hidup. Mahasiswa di jurusan ini akan mempelajari berbagai aspek lingkungan, termasuk konservasi alam, manajemen sumber daya alam, mitigasi bencana, dan pembangunan berkelanjutan. Mereka juga akan belajar tentang dampak aktivitas manusia terhadap lingkungan dan cara mengurangi jejak ekologis.</p>
+    <h2>Prospek Kerja</h2>
+    <p>Lulusan jurusan Lingkungan Hidup memiliki beragam peluang karir di berbagai sektor, termasuk:</p>
+    <ul>
+        <li>Ahli Lingkungan: Menyusun dan melaksanakan program perlindungan lingkungan.</li>
+        <li>Konsultan Lingkungan: Memberikan saran tentang manajemen lingkungan dan keberlanjutan.</li>
+        <li>Peneliti Lingkungan: Melakukan penelitian tentang dampak lingkungan dan solusi yang berkelanjutan</li>
+        <li>Manajer Proyek Lingkungan: Mengelola proyek-proyek lingkungan untuk keberlanjutan.</li>
+    </ul>
+</main>
+<?php include '../structure/footer.php'; ?>
+</body>
+</html>
